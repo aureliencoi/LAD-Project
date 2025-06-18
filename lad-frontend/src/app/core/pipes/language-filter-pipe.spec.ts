e39cf55ff9b30e18974e61360a50e3d1b9bbcd12
aureliencoi <aurelien.coiffard@gmail.com>
2025-06-18 11:25:39 +0200

    chore: Remove old Angular frontend

--- lad-frontend/src/app/core/pipes/language-filter-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LanguageFilterPipe } from './language-filter-pipe';
-
-describe('LanguageFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new LanguageFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
